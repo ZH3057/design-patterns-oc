72e3745d90cc1ba7cf5a24c4bbda6b9abb9b1ea1
zhoujun <zhoujun@knowbox.cn>
2018-06-26 10:37:06 +0800

    project init

---
+++ ZJDesignPatterns/ZJDesignPatterns/ViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJDesignPatterns
+//
+//  Created by Jun Zhou on 2018/6/26.
+//  Copyright © 2018年 Jun Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZJDesignPatterns/ZJDesignPatterns/ViewController/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZJDesignPatterns
+//
+//  Created by Jun Zhou on 2018/6/26.
+//  Copyright © 2018年 Jun Zhou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
